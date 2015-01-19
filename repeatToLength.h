711930d4a747a0fe4eb82240cff0a6d818dd0dd0
Daniel Patru <daniel.patru@samraksh.com>
2015-01-19 09:19:26 -0500

    Comment.

--- repeatToLength.h
+++ repeatToLength.h
@@ -3,11 +3,12 @@ using std::string;
 
 string repeatToLength ( unsigned len, string s ) {
   string r, si = s;
+  // add all the whole multiples of s.
   for ( unsigned q = len / s.size(); q > 0; q >>= 1 ) {
-    if ( q & 1 ) r += si;
-    si +=  si;
+    if ( q & 1 ) r += si; // add si to r if the low bit of q is 1
+    si +=  si; // double si
   }
-  r += s.substr ( 0, len - r.size() );
+  r += s.substr ( 0, len - r.size() ); // add any remainder
   return r;
 }
   
